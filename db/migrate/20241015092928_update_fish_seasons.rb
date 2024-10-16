54526305df7d8cbdd6b8968c304ff119b196bdab
Kuriyama301 <kuriyama.kosuke@gmail.com>
2024-10-16 11:36:07 +0900

    fix/138-upndate-fish-seasons.rb

--- db/migrate/20241015092928_update_fish_seasons.rb
+++ db/migrate/20241015092928_update_fish_seasons.rb
@@ -1,4 +1,13 @@
 class UpdateFishSeasons < ActiveRecord::Migration[7.0]
   def change
+    add_column :fish_seasons, :start_month, :integer
+    add_column :fish_seasons, :start_day, :integer
+    add_column :fish_seasons, :end_month, :integer
+    add_column :fish_seasons, :end_day, :integer
+
+    remove_column :fish_seasons, :start_date, :date
+    remove_column :fish_seasons, :end_date, :date
+    remove_index :fish_seasons, [:fish_id, :start_date, :end_date], if_exists: true
+    add_index :fish_seasons, [:fish_id, :start_month, :start_day, :end_month, :end_day], unique: true, name: 'index_fish_seasons_on_fish_id_and_dates'
   end
 end
