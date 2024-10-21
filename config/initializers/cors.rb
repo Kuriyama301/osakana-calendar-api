fac3ec1e10ae955ad80b183280b24e6523ecdb21
Kuriyama301 <kuriyama.kosuke@gmail.com>
2024-10-21 21:14:23 +0900

    fix/37-heroku-deployment-config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV.fetch('ALLOWED_ORIGINS', 'http://localhost:3000').split(',').map(&:strip)
+    origins 'https://osakana-calendar-frontend.herokuapp.com'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: ENV.fetch('CORS_CREDENTIALS', 'false') == 'true'
+      credentials: true
   end
 end
